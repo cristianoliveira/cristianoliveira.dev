d0d4da61cd711dff533ac8f37bda416968a980f5
Cristian Oliveira <contato@cristianoliveira.com.br>
2019-03-10 16:34:55 +0100

    chore: bash fixes

--- scripts/publish_changes.sh
+++ scripts/publish_changes.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 echo "Building..."
 gatsby build
 
@@ -12,15 +14,14 @@ function create_dir() {
   ftppath=${dir:7}
 
   echo "Verifying $dir exists in ftp://ftp.cristianoliveira.com.br/./$ftppath/"
-  echo $exist
   echo ".keep" > ./public/.keep
-  exists=$(curl --silent --head -u $MYBLOG_FTP_TOKEN ftp://ftp.cristianoliveira.com.br/$ftppath/.keep)
+  exists=$(curl --silent --head -u "$MYBLOG_FTP_TOKEN" "ftp://ftp.cristianoliveira.com.br/$ftppath/.keep")
   echo "exists: $exists"
   if [[ -z "$exists" ]]; then
     echo "Creating directory..."
     echo "$dir ftp://ftp.cristianoliveira.com.br/./$ftppath/"
-    curl "ftp://ftp.cristianoliveira.com.br/./$ftppath/" --ftp-create-dirs -u $MYBLOG_FTP_TOKEN
-    curl -T public/.keep "ftp://ftp.cristianoliveira.com.br/./$ftppath/" --ftp-create-dirs -u $MYBLOG_FTP_TOKEN
+    curl "ftp://ftp.cristianoliveira.com.br/./$ftppath/" --ftp-create-dirs -u "$MYBLOG_FTP_TOKEN"
+    curl -T public/.keep "ftp://ftp.cristianoliveira.com.br/./$ftppath/" --ftp-create-dirs -u "$MYBLOG_FTP_TOKEN"
   fi
   echo "Updated."
 }
@@ -29,12 +30,12 @@ function upload() {
   file=${1}
   ftppath=${file:7}
 
-  exists=$(curl --silent --head -u $MYBLOG_FTP_TOKEN ftp://ftp.cristianoliveira.com.br/$ftppath/.keep)
+  exists=$(curl --silent --head -u "$MYBLOG_FTP_TOKEN" "ftp://ftp.cristianoliveira.com.br/$ftppath/.keep")
   echo "exists: $exists"
   if [[ -z "$exists" ]]; then
     echo "uploading new file..."
     echo "$file ftp://ftp.cristianoliveira.com.br/$ftppath"
-    curl -u $MYBLOG_FTP_TOKEN --ftp-create-dirs -T $file ftp://ftp.cristianoliveira.com.br/$ftppath
+    curl -u "$MYBLOG_FTP_TOKEN" --ftp-create-dirs -T "$file" "ftp://ftp.cristianoliveira.com.br/$ftppath"
   fi
   echo "Updated."
 }
@@ -42,12 +43,12 @@ function upload() {
 
 directories=$(find public -type d | awk '{ print length, $0 }' | sort -n -s | cut -d" " -f2-)
 for d in $directories ; do # Not recommended, will break on whitespace
-  create_dir $d
+  create_dir "$d"
 done
 
 
 files=$(find public -type f | awk '{ print length, $0 }' | sort -n -s | cut -d" " -f2-)
 for f in $files ; do # Not recommended, will break on whitespace
-  upload $f
+  upload "$f"
 done
 
